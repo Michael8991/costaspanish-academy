b59d7f1947b61bed0db24a6afd2cfca4a7a4d78f
Michael Esequiel Rodríguez Iranzo <michael2002982@gmail.com>
2025-11-05 19:26:39 +0100

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://costaspanishclass.com',
+    siteUrl: 'https://www.costaspanishclass.com',
     generateRobotsTxt: true,
     sitemapSize: 7000,
+    exclude: ['/server-sitemap.xml'],
+    alternateRefs: [
+        { href: 'https://www.costaspanishclass.com/es', hreflang: 'es' },
+        { href: 'https://www.costaspanishclass.com/en', hreflang: 'en' },
+    ],
 };
